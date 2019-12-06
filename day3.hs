f7f3b60e889d548b765068ebbb439b53cf534c67
Max Carvajal <jmcarvajal@paypal.com>
2019-12-06 10:00:00 -0600

    In progress commit for day3

---
+++ day3.hs
@@ -0,0 +1,32 @@
+module Day3 where
+
+import System.IO
+import Control.Monad
+
+getTriangles :: FilePath -> IO [String]
+getTriangles path = do
+    contents <- readFile path
+    let triangles = map read . lines $ contents
+    return triangles
+
+countValidTriangles :: IO Int
+countValidTriangles = do
+    triangles <- getTriangles "day3input"
+    return $ length $ filter isValidTriangle triangles
+
+isValidTriangle :: String -> Bool
+isValidTriangle triangleStr = 
+    let 
+        triangle = map (read::String->Int) $ words triangleStr
+    in
+        and [testFirst triangle, testSecond triangle, testThird triangle]
+    where
+        testFirst t = (head t) < (sum $ tail t)
+        testSecond t = (head $ tail t) < (sum [head t, last t])
+        testThird t = (last t) < (sum $ init t)
+
+
+main :: IO ()
+main = do
+    numTriangles <- countValidTriangles
+    putStrLn $ show numTriangles
