195368c87e8cad340a1267d6928a8af17e884e3b
Max Carvajal <jmcarvajal@paypal.com>
2020-01-15 16:22:12 -0600

    in progress commit

--- day8.hs
+++ day8.hs
@@ -13,6 +13,14 @@ printScreen :: [String] -> IO ()
 printScreen screen = do
     putStrLn $ unlines screen
 
+rect :: Int -> Int -> [String] -> [String]
+rect a b screen =
+    let 
+        newRow = ['#' if x<=a else '.' | <- x [1..50]]
+    in
+        blitRow
+            
+
 main :: IO ()
 main = do
     let screen = createBlankScreen
